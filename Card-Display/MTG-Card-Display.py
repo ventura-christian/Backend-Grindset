44b8fabf5cd8f4721f2dd385d2b5cd5b817d238b
[Christian Ventura] <[cventura64@outlook.com]>
2025-09-04 21:33:14 -0700

    Added File
    Built this program to determine if a Magic: the Gathering card inputted by the user is contained  within a list. This program will grow and evolve over time. More features to come in future iterations.

---
+++ Card-Display/MTG-Card-Display.py
@@ -0,0 +1,29 @@
+"""
+This script will store a list of card names. When the user enters a card name, it will determine if the card is in the list.
+- Create a function to store list of card names.
+- Add logic to determine if the requested card is in the list. 
+- Create main function
+- Add a prompt message for the user along with functionality to exit or continue the program based on user input. 
+- Run program and test. 
+"""
+
+def card_database(cardname):
+    card_list = [
+        "Bloodstained Mire", "Flooded Strand", "Polluted Delta", "Windswept Heath", "Wooded Foothills", "Arid Mesa", "Misty Rainforest", "Scalding Tarn", "Verdant Catacombs", "Marsh Flats", "Black Lotus", "Ancestral Recall", "Time Walk", "Timetwister", "Mox Pearl", "Mox Sapphire", "Mox Jet", "Mox Ruby", "Mox Emerald"
+    ]
+    
+    if cardname in card_list:
+        return f"{cardname} is in the list!\n"
+    else:
+        return f"{cardname} is not in the list.\n"
+    
+def main():
+    print()
+    card_choice = input('Please enter a Magic: the Gathering card name\n---------------------------------------------\n>>')
+    if card_choice != 'exit':
+        print(card_database(card_choice))
+        return main()
+    else:
+        print('\nThank you for using this program!')
+    
+main()
