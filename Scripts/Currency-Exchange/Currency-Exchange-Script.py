1f6877674c5744b76b9f1921e91de20884aebb59
[Christian Ventura] <[cventura64@outlook.com]>
2025-09-12 10:39:25 -0700

    Added a module-level docstring to the document and finished the last function. This was created with instructions from exercism.org

--- Scripts/Currency-Exchange/Currency-Exchange-Script.py
+++ Scripts/Currency-Exchange/Currency-Exchange-Script.py
@@ -1,40 +1,44 @@
-# def exchange_money(budget, exchange_rate):
-#     result = float(budget / exchange_rate)
-#     return result
+"""
+Currency Exchange Utility Functions
+
+This script provides functions to:
+- Calculate exchanged currency based on a budget and exchange rate (with optional spread/fee).
+- Determine change after exchanging money.
+- Compute the value and number of bills received for a given denomination.
+- Find leftover currency after exchanging into bills.
+- Calculate the maximum exchangeable value in whole bills, considering the spread and denomination.
+
+Useful for simulating or automating currency exchange scenarios.
+"""
+
+def exchange_money(budget, exchange_rate):
+    result = float(budget / exchange_rate)
+    return result
 
 
-# def get_change(budget, exchanging_value):
-#     result = float(budget - exchanging_value)
-#     return result
+def get_change(budget, exchanging_value):
+    result = float(budget - exchanging_value)
+    return result
 
 
-# def get_value_of_bills(denomination, number_of_bills):
-#     result = int(denomination * number_of_bills)
-#     return result
+def get_value_of_bills(denomination, number_of_bills):
+    result = int(denomination * number_of_bills)
+    return result
 
 
-# def get_number_of_bills(amount, denomination):
-#     result = int(amount // denomination)
-#     return result
+def get_number_of_bills(amount, denomination):
+    result = int(amount // denomination)
+    return result
 
 
-# def get_leftover_of_bills(amount, denomination):
-#     result = float(amount % denomination)
-#     return result
+def get_leftover_of_bills(amount, denomination):
+    result = float(amount % denomination)
+    return result
 
-# LEFT OFF HERE. Function not completed. 
 def exchangeable_value(budget, exchange_rate, spread, denomination):
-    result = int(budget * exchange_rate) + spread / denomination
-    spread = spread / 100
-    return int(result)
+    actual_rate = exchange_rate + (exchange_rate * spread / 100)
+    exchanged = budget / actual_rate
+    number_of_bills = int(exchanged // denomination)
+    return number_of_bills * denomination
     
 
-    """
-    :param budget: float - the amount of your money you are planning to exchange.
-    :param exchange_rate: float - the unit value of the foreign currency.
-    :param spread: int - percentage that is taken as an exchange fee.
-    :param denomination: int - the value of a single bill.
-    :return: int - maximum value you can get.
-    """
-
-print(exchangeable_value(127.25, 1.20, 10, 20))
