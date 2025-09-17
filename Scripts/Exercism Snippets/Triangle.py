3af40bf28760d183c02ed9f0ff3ea2daf027428a
Christian Ventura <cventura64@outlook.com>
2025-09-17 10:30:52 -0700

    Abandoned script, waste of time.

--- Scripts/Exercism Snippets/Triangle.py
+++
@@ -1,8 +0,0 @@
-"""
-Instructions: 
-- Determine if a triangle is equilateral, isosceles, or scalene.
-- Equilateral triangle: three sides with the same lengths.
-- Isosceles triangle: at least two sides with the same length.
-- Scalene triangle: has al sides of different lengths. 
-"""
-
