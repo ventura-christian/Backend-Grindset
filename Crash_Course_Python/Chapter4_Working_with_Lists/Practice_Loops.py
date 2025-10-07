2a17699a42a5a4e29bbfef6cfc32c27d8af4d9d1
Christian Ventura <cventura64@outlook.com>
2025-10-07 12:04:55 -0700

    These are two exercise examples that I wrote with information I gained regarding for loops from both chapters 3 and 4 of Python Crash Course.

---
+++ Crash_Course_Python/Chapter4_Working_with_Lists/Practice_Loops.py
@@ -0,0 +1,19 @@
+"""
+This will hold the practice exercise problems for Chapter 4; Python Crash Course
+* 4-1: Pizzas [Think of at least three kinds of your favorite pizza. Store these pizza names in a list, and then use a for loop to print the name of each pizza.]
+* 4-2: Animals [Think of at least three different animals that have a common characteristic. Store the names of these animals in a list, and then use a for loop to print out the name of each animal.]
+"""
+
+""" 4-1
+favorite_types_of_pizza = ['veggie', 'jalapeno/mushroom', 'sausage/onion']
+for pizza in favorite_types_of_pizza:
+    print(f'{pizza.title()} pizza is bomb, I could eat this every day!')
+print('Those are only a few of my favorite types of pizzas to eat.')
+"""
+
+""" 4-2
+types_of_felines = ['tiger', 'leopard', 'jaguar', 'lion']
+for feline in types_of_felines:
+    print(f'A {feline.title()} is a wild animal and is very dangerous, don\'t keep this as a pet!')
+print('All of these animals belong to "Felidae", which is a family of mammals in the order Carnivora colloquially referred to as cats.')
+"""
